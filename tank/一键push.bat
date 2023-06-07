3f63a8013bf35d4662b5e7195c49cac995e721ae
hansionpi <632938843@qq.com>
2023-06-07 18:47:36 +0800

    Site updated: 2023-06-07 18:47:34

---
+++ tank/一键push.bat
@@ -0,0 +1,4 @@
+cd D:/tankvideo/
+git add .
+git commit -m "add commit from Widows."
+git push origin master
---
+++ tank/一键上传.sh
@@ -0,0 +1,4 @@
+cd /Users/MySpace/MySpace/tankvideo/
+sudo git add .
+sudo git commit -m "add commit from mac os."
+sudo git push origin master
